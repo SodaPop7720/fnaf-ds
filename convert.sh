0c2457f795c33a5e0a7fe813d490e44c4f2e6da3
SodaPop7720 <123225358+SodaPop7720@users.noreply.github.com>
2026-04-13 10:34:28 -0600

    Delete convert.sh

--- convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BLOCKSDS="${BLOCKSDS:-/opt/blocksds/core/}"
-GRIT=$BLOCKSDS/tools/grit/grit
-
-$GRIT officePowerOut.png -ftB -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
-$GRIT officePowerOutFreddy.png -ftB -fh! -gTFF00FF -gt -gB8 -mR8 -mLs
-
-mv *.pal *.img *.map nitrofiles/bg
